89773b4f51ed4f82569c3f6b5e7e4c186d778a40
Ollie Davis <olliehd1322@gmail.com>
2021-04-12 05:59:54 +0100

    comments added/updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,25 @@
 class ArticlesController < ApplicationController
-  # shows all posts | GET
+  # shows all posts | HTML GET REQUEST
   def index
     @articles = Article.all
   end
 
-  # shows a specific post | GET
-  def show 
+  # shows the specified post | HTML GET REQUEST
+  def show
     @article = Article.find(params[:id])
   end
 
-  # shows the 'new' post view | GET
+  # shows the 'new' post view | HTML GET REQUEST
   def new
     @article = Article.new
   end
 
+  # shows the specified post page to be edited | HTML GET REQUEST
   def edit
     @article = Article.find(params[:id])
   end
 
-  # creates the new post and redirects to the new post | POST
+  # creates the new post and redirects to the new post | HTML POST REQUEST
   def create
     @article = Article.new(article_params)
 
