0d0920aa69770b4f8a57a2de4a07c0e02ce0229c
Ollie Davis <olliehd1322@gmail.com>
2021-04-12 06:59:03 +0100

    Destroy action added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,6 +43,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # delete/destroy the specified post and redirects to the index | HTML DELETE REQUEST
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
