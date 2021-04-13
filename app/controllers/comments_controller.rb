6b61c261cac9351c1e5f847a3745ebe145ff63df
Ollie Davis <olliehd1322@gmail.com>
2021-04-13 06:15:06 +0100

    Comments added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
   http_basic_authenticate_with name: 'Ollie', password: 'supersecretplaintextpw', only: :destroy
 
+  # creates the new comment and redirects to the post | HTML POST REQUEST
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
+  # delete/destroy the specified comment and redirects to the index | HTML DELETE REQUEST
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
